68476dde145102262bc1d1ad713098275a2199ac
Fahim Rahman <fahimpranto002@gmail.com>
2020-04-13 17:13:15 +0600

    compressed items

--- js/ajax-mail.js
+++ js/ajax-mail.js
@@ -1,1 +1,37 @@
-!function(a){"use strict";a("#contact-form .input_field.required").on("blur",function(){""==a(this).val().trim()?a(this).addClass("inputEmpty").removeClass("inputNotEmpty"):a(this).removeClass("inputEmpty").addClass("inputNotEmpty"),""==a(this).val().trim()&&a(this).attr("placeholder","Field is Required!")});var b=a("#contact-form"),c=a(".form-message");a(b).submit(function(d){d.preventDefault();var e=a(b).serialize();a.ajax({type:"POST",url:a(b).attr("action"),data:e}).done(function(b){a(c).removeClass("error"),a(c).addClass("success"),a(c).text(b),a("#contact-form input,#contact-form textarea").val("")}).fail(function(b){a(c).removeClass("success"),a(c).addClass("error"),a(c).text(""!==b.responseText?b.responseText:"Oops! An error occured and your message could not be sent.")})})}(jQuery);
+(function ($) {
+	"use strict";
+
+    // form validation
+    $('#contact-form .input_field.required').on('blur', function () {
+		if ($(this).val().trim() == '') { $(this).addClass('inputEmpty').removeClass('inputNotEmpty'); }
+		else { $(this).removeClass('inputEmpty').addClass('inputNotEmpty'); }
+		if ($(this).val().trim() == '') { $(this).attr('placeholder', 'Field is Required!'); }
+    });
+	
+	var form = $('#contact-form');
+	var formMessages = $('.form-message');
+	$(form).submit(function(e) {
+		e.preventDefault();
+		var formData = $(form).serialize();
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+			$(formMessages).text(response);
+			$('#contact-form input,#contact-form textarea').val('');
+		})
+		.fail(function(data) {
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+	});
+})(jQuery);
